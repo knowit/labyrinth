a23ac72eb6b9d3673abc9d37b0f54bbf6298a20d
Andreas Bade <badeand@badeand.com>
2019-04-10 18:49:49 +0200

    added libraries

---
+++ testprograms/acctest/arduino/gamecontroller/.piolibdeps/DisplayCore_ID1024/DisplayCore_Reading.cpp
@@ -0,0 +1,20 @@
+#include <DisplayCore.h>
+#include <stdarg.h>
+
+/*!
+ *  Returns the colour at (x,y) as seen by the screen.
+ *
+ *  Example:
+ *
+ *      unsigned int color = tft.colorAt(100, 100);
+ */
+color_t DisplayCore::colorAt(int __attribute__((unused)) x, int __attribute__((unused)) y) {
+    return bgColor;
+}
+
+void DisplayCore::getRectangle(int __attribute__((unused)) x, int __attribute__((unused)) y, int w, int h, color_t *buf) {
+    for (int i = 0; i < w*h; i++) {
+        buf[i] = bgColor;
+    }
+}
+
