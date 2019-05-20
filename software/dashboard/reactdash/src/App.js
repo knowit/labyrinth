e4d1b1760fd7dd2e001e261e94ea397096332cde
Andreas Bade <badeand@badeand.com>
2019-05-20 20:51:51 +0200

    corrected gitignore

---
+++ software/dashboard/reactdash/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import GameStatus from './GameState';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GameStatus />
+    </div>
+  );
+}
+
+export default App;
