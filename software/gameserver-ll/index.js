265a7358f780391db5f881ec3eff2cd629ac21a3
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-06-01 16:03:24 +0200

    added serial port mock and improved callbacks

--- software/gameserver-ll/index.js
+++ software/gameserver-ll/index.js
@@ -1,6 +1,22 @@
 const GameController = require('../gamecontroller/js/')
 const net = require('net')
 
+const quatToEulerAxis = (q) => {
+	const x = Math.atan2(
+        2.0 * (q.w * q.x + q.y * q.z),
+        1.0 - 2.0 * (q.x * q.x + q.y * q.y));
+
+    const sinp = 2.0 * (q.w * q.y - q.z * q.x);
+    const y = Math.abs(sinp) >= 1 ? Math.sign(sinp) * (Math.PI / 2.0) :  Math.asin(sinp)
+ 
+    const z = Math.atan2(
+        2.0 * (q.w * q.z + q.x * q.y), 
+        1.0 - 2.0 * (q.y * q.y + q.z * q.z));
+    
+    // Radians
+    return { x, y, z }
+}
+
 const controller = new GameController()
 const portName = process.argv[2]
 
@@ -23,10 +39,13 @@ const server = net.createServer(socket => {
                 // onYSpeed
             });
 
-    socket.on('data', (data) => {
+    socket.on('data', (buffer) => {
+        var recieved = JSON.parse(buffer.toString('ascii'));
         
-        // controller.setXAngle(value * 5);
-        // controller.setYAngle(value * 5);
+        var orientation = quatToEulerAxis(recieved.Rotation);
+
+        controller.setXAngle(orientation.x);
+        controller.setYAngle(orientation.z);
     })
 })
 server.on('error', err => console.error(err))
